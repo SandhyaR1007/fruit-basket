0a08a5d67e07781dd4e95c9570b11d529b2a5c46
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-09-07 22:58:17 +0530

    add basic structure of fruit identification, object with fruit emojis with their names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React from "react";
-
+import "./App.css";
+import { useState } from "react";
+import { fruits } from "./fruits";
 const App = () => {
-  return <div>App</div>;
+  const [search, setSearch] = useState("");
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("");
+  const checkCart = (val) => {
+    if (Object.keys(fruits).includes(val)) {
+      setName(fruits[val].name);
+      setName(fruits[val].color);
+    } else if (val == "") {
+      setName("");
+    } else {
+      console.log(Object.values(fruits));
+      setColor("");
+      setName("Looks like we don't have this fruit !!");
+    }
+  };
+  return (
+    <div className="App" style={{ background: `${color}` }}>
+      <h1>Fruit Basket</h1>
+      <input
+        value={search}
+        placeholder="Enter fruit to search"
+        onChange={(e) => {
+          setSearch(e.target.value);
+          checkCart(e.target.value);
+        }}
+      />
+      <p className="sub-head">fruits we have</p>
+      <div className="basket">
+        {Object.keys(fruits).map((i, index) => (
+          <span
+            className="fruit"
+            key={index}
+            onClick={() => {
+              setColor(fruits[i].color);
+              setSearch(i);
+              setName(fruits[i].name);
+            }}
+          >
+            {i}
+          </span>
+        ))}
+      </div>
+      <div className="result">{name}</div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/fruits.js
@@ -0,0 +1,12 @@
+export const fruits = {
+  "🍇": { name: "grapes", color: "rgba(163, 93, 234, 0.5)" },
+  "🥭": { name: "mango", color: "rgba(246, 185, 59, 0.5)" },
+  "🍌": { name: "banana", color: "rgba(246, 229, 141, 0.5)" },
+  "🍎": { name: "apple", color: "rgba(229, 80, 57, 0.5)" },
+  "🍉": { name: "watermelon", color: "rgba(5, 196, 107, 0.5)" },
+  "🍓": { name: "strawberry", color: "rgba(255, 107, 129, 0.5)" },
+  "🍍": { name: "pineapple", color: "rgba(234, 181, 67, 0.5)" },
+  "🍊": { name: "orange", color: "#rgba(250, 130, 49, 0.5)" },
+  "🍐": { name: "pear", color: "rgba(184, 233, 148, 0.5)" },
+  "🥝": { name: "kiwi", color: "rgba(120, 224, 143, 0.5)" },
+};
